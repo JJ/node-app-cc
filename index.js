1510f8ea52f1dbe30978dbe4d2f729a22802cec5
JJ Merelo <jjmerelo@gmail.com>
2014-12-10 18:53:12 +0100

    Interfaz mínimo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+// recuerda ejecutar antes grunt creadb
 var db_file = "porrio.db.sqlite3";
 var apuesta = require("./Apuesta.js");
 var porra = require("./Porra.js");
@@ -8,14 +9,22 @@ var porra = require("./Porra.js");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(db_file);
 
-var nueva_porra = new porra.Porra('Polopos','Alhama','Copa D', 2014);
-nueva_porra.inserta_db( db, 'partido');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send( nueva_porra );
+app.put('/porra/:local/:visitante/:competition/:year', function( req, response ) {
+    var nueva_porra = new porra.Porra(req.params.local,req.params.visitante,
+				      req.params.competition, req.params.year );
+    nueva_porra.inserta_db( db, 'partido');
+    response.send(nueva_porra)
+});
+
+app.get('/porras', function(request, response) {
+    db.all("SELECT * FROM partido", function(err, rows) {
+	response.send( rows );
+    });
+    
 });
 
 app.listen(app.get('port'), function() {
