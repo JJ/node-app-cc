67618f778ea386cda8f936352b41c9fdacd24953
JJ Merelo <jjmerelo@gmail.com>
2014-12-30 10:07:52 +0100

    Cambiando el interfaz de apuestas
    Y mejorando los tests de camino

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var apuesta = require("./Apuesta.js");
 var porra = require("./Porra.js");
 
-var porras = new Array;
+var porras = new Object;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -13,9 +13,14 @@ app.use(express.static(__dirname + '/public'));
 app.put('/porra/:local/:visitante/:competition/:year', function( req, response ) {
     var nueva_porra = new porra.Porra(req.params.local,req.params.visitante,
 				      req.params.competition, req.params.year );
-    porras.push(nueva_porra);
-    response.send(nueva_porra);
+    porras[nueva_porra.ID] = nueva_porra;
+    response.send( nueva_porra );
 });
+
+/* 
+app.put('/apuesta/:competition/:year/:local/:goles_local/:visitante/:goles_visitante', function( req, response ) {
+    
+}); */ 
 
 app.get('/porras', function(request, response) {
     response.send( porras );
--- test/app.js
+++ test/app.js
@@ -1,11 +1,25 @@
 var request = require('supertest'), 
+should = require('should'),
 app = require('../index.js');
 
 describe( "PUT porra", function() {
-    it('should create', function (done) {
+    it('should return correct type', function (done) {
 	request(app)
 	    .put('/porra/uno/dos/tres/4')
 	    .expect('Content-Type', /json/)
 	    .expect(200,done);
+    });
+    it('should return ID', function (done) {
+	request(app)
+	    .put('/porra/mad/bcn/liga/2014')
+	    .expect('Content-Type', /json/)
+	    .expect(200)
+	    .end( function ( error, resultado ) {
+		if ( error ) {
+		    return done( error );
+		}
+		resultado.body.should.have.property('ID',"mad-bcn-liga-2014");
+		done();
+	    });
     });
 });
