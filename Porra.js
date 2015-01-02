873ad228bbf572e3b8cba60fc566e717149602db
JJ Merelo <jjmerelo@gmail.com>
2015-01-02 22:43:06 +0100

    Añade función y test

--- Porra.js
+++ Porra.js
@@ -4,18 +4,22 @@ exports.Porra = function (local,visitante,competition,year) {
     this.visitante= visitante;
     this.competition= competition;
     this.year = year;
-    this.apuestas = new Array;
+    this.apuestas = new Object;
     this.ID = crea_id( local,visitante,competition,year );
     // functions
     this.nueva_apuesta = nueva_apuesta;
     this.las_apuestas = las_apuestas;
     this.inserta_db = inserta_db;
     this.crea_id = crea_id;
-    
+    this.apuestas_para = apuestas_para;
 }
 
 function nueva_apuesta( apuesta ) {
-    this.apuestas.push( apuesta );
+    var resultado = apuesta.local + "-" + apuesta.visitante;
+    if ( !this.apuestas[ resultado ] ) {
+	this.apuestas[ resultado ] = [];
+    }
+    this.apuestas[ resultado ].push( apuesta );
 }
 
 function crea_id ( local, visitante, competition, year ) {
@@ -24,6 +28,10 @@ function crea_id ( local, visitante, competition, year ) {
 
 function las_apuestas () {
     return this.apuestas;
+}
+
+function apuestas_para ( resultado ) {
+    return this.apuestas[ resultado ];
 }
 
 function inserta_db( db, tabla ) {
