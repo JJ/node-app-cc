9541f9050abdccdfc945fe027df2d26ce0843eb1
JJ Merelo <jjmerelo@gmail.com>
2015-01-03 14:31:20 +0100

    Reasignación IP para que vaya en Heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var porra = require("./Porra.js");
 var porras = new Object;
 
 // Establece el IP y el puerto dependiendo del PaaS que sea
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'; 
 app.set('port', (process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
--- test-with-url.sh
+++ test-with-url.sh
@@ -7,9 +7,15 @@ if [[ "${URL}" == "" ]]; then
 fi
 
 echo "Crea porra"
-curl -X PUT "${URL}/porra/SEV/BET/Copa/2012"
-echo "Crea apuesta"
+curl -X PUT "${URL}/porra/Copa/2012/SEV/BET"
+echo "Crea apuestas"
 curl -X PUT "${URL}/apuesta/DN/Copa/2012/SEV/2/BET/2"
+curl -X PUT "${URL}/apuesta/DU/Copa/2012/SEV/3/BET/2"
+curl -X PUT "${URL}/apuesta/DK/Copa/2012/SEV/2/BET/3"
+curl -X PUT "${URL}/apuesta/KD/Copa/2012/SEV/2/BET/2"
 echo "Recupera apuestas"
 curl "${URL}/porras"
-
+echo "Pone resultado"
+curl -X POST "${URL}/porra/resultado/Copa/2012/SEV/2/BET/2"
+echo "Recupera ganador"
+curl  "${URL}/porra/ganador/Copa/2012/SEV/BET"
