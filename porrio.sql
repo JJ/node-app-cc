4966a0ab088936bc5bbd33878dd37488246cd1f9
JJ Merelo <jjmerelo@gmail.com>
2015-01-10 11:08:42 +0100

    SQL milagroso

--- porrio.sql
+++ porrio.sql
@@ -1,9 +1,9 @@
-CREATE TABLE apuesta(partido varchar(50), 
+CREATE TABLE IF NOT EXISTS  apuesta(partido varchar(50), 
        quien_apuesta varchar(50) not null, 
        goles_local  int not null,
        goles_visitante  int not null);
 
-CREATE TABLE partido( id varchar(50) not null primary key,
+CREATE TABLE IF NOT EXISTS partido( id varchar(50) not null primary key,
        equipo_local varchar(50) not null,
        equipo_visitante varchar(50) not null, 
        competicion varchar(20)  not null,
