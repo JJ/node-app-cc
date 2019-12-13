e6baeb1a86dc0d634f5af8bf8cb08cf5029ef197
JJ Merelo <jjmerelo@gmail.com>
2019-12-13 17:49:07 +0100

    Terminado cambio a SSOT closes #4

--- test/test_porras.js
+++ test/test_porras.js
@@ -1,11 +1,14 @@
 var should = require("should"),
     porra = require(__dirname+"/../lib/Porra.js");
 
-const { Porras } =  require(__dirname+"/../lib/Porras.js");
+
+const { FSDator } =  require(__dirname+"/../lib/FSDator.js");
+const { Porras }  =  require(__dirname+"/../lib/Porras.js");
 
 // Variable global
+const dator = new FSDator();
 var esta_porra = new porra.Porra( "Marajena", "Gualchos", "Provincial", "2014"),
-    estas_porras = new Porras();
+    estas_porras = new Porras( dator );
 
 
 // si se quiere probar la base de datos, ejecutar antes `grunt creadb`
@@ -28,7 +31,7 @@ describe('Porras', function(){
       });
       it('Debería añadir nueva apuesta a la porra', function( done ) {
 	  estas_porras.apuesta(esta_porra, "XYZ", 2,2 );
-	  should.deepEqual(estas_porras.porra(esta_porra.ID).apuestas["2-2"],['XYZ']);
+	  estas_porras.porra(esta_porra.ID).apuestas['XYZ'].should.not.be.null;
 	  done();
       });
   });
@@ -38,7 +41,7 @@ describe('Porras', function(){
 	 estas_porras.nueva( esta_porra );
 	 const porra = estas_porras.porra(esta_porra.ID)
 	 porra.should.not.be.null;
-	 should.deepEqual( porra, esta_porra, "Insertado igual a almacenado" );
+	 porra.ID.should.be.eql( esta_porra.ID );
 	 estas_porras.resultado( esta_porra.ID, 2,2 );
 	 var winners = estas_porras.ganadores(esta_porra.ID);
 	 should.deepEqual(winners,['XYZ']);
