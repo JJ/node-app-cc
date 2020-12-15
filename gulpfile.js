73b9282fdc6362a20099c94299e6937eccf0caf2
JJ Merelo <jjmerelo@gmail.com>
2020-12-15 10:12:05 +0100

    Convierte en asíncrono

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,28 @@ const mocha = require('gulp-mocha');
 const pm2   = require('pm2');
 var shell = require('gulp-shell');
 
-gulp.task('test', () => (
-    gulp.src('test/test_*.js', {read: false})
-    .pipe(mocha({reporter: 'nyan'}))
-));
+gulp.task('test', async () => {
+  gulp.src('test/test_*.js', {read: false})
+  .pipe(mocha({reporter: 'nyan'}))
+});
 
-gulp.task('start', function () {
-  pm2.connect(true, function () {
+gulp.task('start', async () => {
+  pm2.connect(true, async () => {
     pm2.start({
       name: 'Porra',
       script: 'lib/index.js',
       exec_mode: 'cluster',
-      instances: 14
-    }, function () {
+      instances: 4
+    }, async () => {
          console.log('Arranca porra');
-         pm2.streamLogs('all', 0);
        });
   });
 });
 
-gulp.task('stop', shell.task(['pm2 stop Porra' ]));
+gulp.task('stop', async () => {
+  pm2.connect(true, async () => {
+    pm2.stop( "Porra", ( err, proc ) => {
+      console.log("Parando la porra");
+    });
+  });
+});
